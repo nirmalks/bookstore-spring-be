ba428c1c8439c5f1263283317a3e6e020a07d691
nirmalks <kagosekar@gmail.com>
2025-01-10 23:48:23 +0530

    Add missed orderitemrequest

--- src/main/java/com/nirmalks/bookstore/order/api/OrderItemRequest.java
+++ src/main/java/com/nirmalks/bookstore/order/api/OrderItemRequest.java
@@ -1,2 +1,31 @@
-package com.nirmalks.bookstore.order.api;public class OrderItemRequest {
+package com.nirmalks.bookstore.order.api;
+
+public class OrderItemRequest {
+    private Long bookId;
+    private int quantity;
+    private double price;
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
